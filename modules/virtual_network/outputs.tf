dc37e381aaa824842341de790923ed7b4f801549
nicolasfds <nicolas.furtadodasilva@gmail.com>
2025-09-19 22:38:34 +0200

    second terraform fmt

--- modules/virtual_network/outputs.tf
+++ modules/virtual_network/outputs.tf
@@ -6,10 +6,8 @@ output "virtual_network" {
       for k, subnet in var.subnets : k => {
         subnet_id : azurerm_subnet.subnet[k].id
         address_prefix : azurerm_subnet.subnet[k].address_prefixes
-
         network_security_group_id : azurerm_network_security_group.these[k].id
         network_security_group_name : azurerm_network_security_group.these[k].name
-
         route_table_id : azurerm_route_table.these[k].id
         route_table_name : azurerm_route_table.these[k].name
       }
