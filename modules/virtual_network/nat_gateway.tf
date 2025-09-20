de0b8c4077c6349307707d2254c03e6d49e2319f
nicolasfds <nicolas.furtadodasilva@gmail.com>
2025-09-20 14:21:04 +0200

    testing wk

--- modules/virtual_network/nat_gateway.tf
+++ modules/virtual_network/nat_gateway.tf
@@ -14,7 +14,7 @@ resource "azurerm_nat_gateway" "this" {
   resource_group_name     = local.resource_group_name
   location                = var.location
   tags                    = local.tags
-  sku_name                = "Standard"
+  sku_name     = "Standard"
   idle_timeout_in_minutes = 10
 }
 
