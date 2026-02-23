e62d48dbfa3e6303d371df2987ded3f2588bb160
nicolas-furtadods <nicolas.furtadodasilva@gmail.com>
2026-02-23 15:02:53 +0100

    Added NSG Templates

--- modules/virtual_network/rules.tf
+++ modules/virtual_network/rules.tf
@@ -1,5 +1,5 @@
 module "templates" {
-  source = "git::https://github.com/nicolas-furtadods/azure-terraform-playground.git//modules/virtual_network?ref=fastapi"
+  source = "git::https://github.com/nicolas-furtadods/azure-terraform-playground.git//modules/custom_nsg_rules_template?ref=fastapi"
   for_each = {
     for k, subnet in var.subnets : k => subnet if var.template_folder != null && var.template_folder != "" && subnet.template != null && subnet.template != ""
   }
