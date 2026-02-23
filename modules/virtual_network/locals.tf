f5a37811c79c10e97ac5868864703c1042451727
nicolas-furtadods <nicolas.furtadodasilva@gmail.com>
2026-02-23 15:06:58 +0100

    Added NSG Templates

--- modules/virtual_network/locals.tf
+++ modules/virtual_network/locals.tf
@@ -20,4 +20,9 @@ locals {
 
   #Resource Group Name
   resource_group_name = var.resource_group_name == null ? azurerm_resource_group.this[0].name : var.resource_group_name
+
+  nsg_files       = fileset(var.template_folder, "*.json")
+  template_map = {
+    for k, f in local.nsg_files : replace(k, ".json", "") => file("${var.template_folder}/${f}")
+  }
 }
